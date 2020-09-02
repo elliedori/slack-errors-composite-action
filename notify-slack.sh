44716eeec20f1dfe7780e678ed6fe403288994d7
Ellie Bahadori <elliedori@gmail.com>
2020-09-02 15:59:58 -0700

    Add basic notification

---
+++ notify-slack.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Test message from webhook", attachments: [{ "color": "danger", "fields": { "title": "Github Actions”, “value”: “Sample build failure notification" }}]}' ${{ inputs.slack-webhook-url }}
